ec768124a4191fd7a563c521fee58bd56721a3bc
alveflo <victor.alveflo@gmail.com>
2021-04-28 20:30:04 +0200

    Update problem 1, Add problem2

--- problem1.clj
+++ problem1.clj
@@ -1,11 +1,26 @@
+; If we list all the natural numbers below
+; 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+; The sum of these multiples is 23.
+
+; Find the sum of all the multiples of 3 or 5 below 1000.
+
 (ns clojure.examples.hello
     (:gen-class))
 
 (defn hello-world []
-    (dotimes [i 10]
-      (if (or (= 0 (mod i 3)) (= 0 (mod i 5)))
-        (println i)
+;    (def lst (map inc (take 10 (range))))
+
+    (println (->> (range)
+         (take 10)
+         (map inc)))
+
+    (dotimes [i 9]
+      (def y (inc i))
+      (if (or (= 0 (mod y 3)) (= 0 (mod y 5)))
+        (println y)
+        ()
       )
   )
 )
+
 (hello-world)
